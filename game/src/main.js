2c1a1865d739102d0d7b38e63144b2be38c73cb5
Shikazu777 <dhanrax@gmail.com>
2026-01-11 03:46:23 +0530

    update1

--- game/src/main.js
+++ game/src/main.js
@@ -2,9 +2,16 @@ import Phaser from "phaser"
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  backgroundColor: "#1d1d1d",
+  width: window.innerWidth,
+  height: window.innerHeight,
+  backgroundColor: "#d36060",
+
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  },
   scene: {
     preload,
     create,
@@ -18,13 +25,36 @@ let player
 let cursors
 
 function preload() {
-  this.load.image("player", "/player.png")
+  this.load.spritesheet("player", "player.png", {
+    frameWidth: 32,
+    frameHeight: 32
+  })
+  console.log("loading sprite")
 }
 
+
 function create() {
-  player = this.physics.add.sprite(400, 300, "player")
+  player = this.physics.add.sprite(400, 300, "player", 0)
+  this.cameras.main.startFollow(player)
+  this.cameras.main.setZoom(4)
+
+
+  this.anims.create({
+    key: "walk",
+    frames: this.anims.generateFrameNumbers("player", {
+      start: 0,
+      end: 3
+    }),
+    frameRate: 8,
+    repeat: -1
+  })
+
+  player.play("walk")
+
   cursors = this.input.keyboard.createCursorKeys()
+  console.log("sprite created")
 }
+
 
 function update() {
   player.setVelocity(0)
